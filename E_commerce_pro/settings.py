60a2c875c3ce240aa96b70418a9b054ddca50883
rabee361 <rha60540@gmail.com>
2023-10-19 00:21:05 +0300

    changes

--- E_commerce_pro/settings.py
+++ E_commerce_pro/settings.py
@@ -146,7 +146,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'base/static'
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'base/staticfiles') 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
