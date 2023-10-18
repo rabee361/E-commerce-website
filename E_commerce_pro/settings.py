5459fce27e0b46c91aec568447894f405d7efb5b
rabee361 <rha60540@gmail.com>
2023-10-18 21:27:01 +0300

    changes

--- E_commerce_pro/settings.py
+++ E_commerce_pro/settings.py
@@ -42,7 +42,7 @@ SECRET_KEY = get_env_variable('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','.now.sh']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
